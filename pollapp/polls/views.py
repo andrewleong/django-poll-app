bb71f7744470ba2f02044442cec1080c202396cd
andrew leong <andrew.leong@jewelpaymentech.com>
2019-05-12 22:19:50 +0800

    Add more views for polls

--- pollapp/polls/views.py
+++ pollapp/polls/views.py
@@ -4,6 +4,19 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+
 def index(request):
     return HttpResponse("Hello World")
+
+
+def detail(request, question_id):
+    return HttpResponse("You looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You voting on question %s." % question_id)
