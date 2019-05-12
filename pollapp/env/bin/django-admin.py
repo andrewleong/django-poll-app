1dadd181fdcf0b0b5adea8a0e8790408d7d661d2
andrew leong <andrew.leong@jewelpaymentech.com>
2019-05-12 21:09:53 +0800

    Add poll question and choice

---
+++ pollapp/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/andrewleong/Documents/test-projects/django-poll-app/pollapp/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
